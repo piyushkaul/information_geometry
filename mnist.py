0e95a75ccc0c734395aaab3818e6db1dff6cd457
piyushkaul <piyushkaul@gmail.com>
2020-06-19 20:53:40 +0530

    Added plots and output save etc.

--- mnist.py
+++ mnist.py
@@ -65,7 +65,7 @@ class NGD(Optimizer):
                 if whitening_matrices_list:
                     #print('idx = {}, whitening_matrices[{}].shape, whitening_matrices[{}].shape'.format(idx, whitening_matrices_list_keys[2*idx],whitening_matrices_list_keys[2*idx-1]))
                     psi = whitening_matrices_list_values[2 * idx - 1]
-                    gamma = whitening_matrices_list_values[2 * idx ]
+                    gamma = whitening_matrices_list_values[2 * idx]
                     d_p = psi @ dp @gamma
                 '''
                 if weight_decay != 0:
@@ -119,17 +119,23 @@ class MLP(nn.Module):
         self.linear2 = nn.Linear(250, 100)
         self.linear3 = nn.Linear(100, 10)
         self.GS = OrderedDict()
-        self.GS['PSI0_AVG'] = np.zeros((784, 784))
-        self.GS['GAM0_AVG'] = np.zeros((250,250))
-        self.GS['PSI1_AVG'] = np.zeros((250, 250))
-        self.GS['GAM1_AVG'] = np.zeros((100, 100))
-        self.GS['PSI2_AVG'] = np.zeros((100, 100))
-        self.GS['GAM2_AVG'] = np.zeros((10, 10))
+        self.GS['PSI0_AVG'] = np.eye((784))
+        self.GS['GAM0_AVG'] = np.eye((250))
+        self.GS['PSI1_AVG'] = np.eye((250))
+        self.GS['GAM1_AVG'] = np.eye((100))
+        self.GS['PSI2_AVG'] = np.eye((100))
+        self.GS['GAM2_AVG'] = np.eye((10))
         self.GSLOWER = {}
+        for key, val in self.GS.items():
+            self.GSLOWER[key] = np.eye(self.get_subspace_size(self.GS[key].shape[0]))
         self.GSINV = {}
         self.P = {}
         for item_no, (key, item) in enumerate(self.GS.items()):
             self.GSINV[key] = self.GS[key]
+            subspace_size = self.get_subspace_size(self.GSINV[key].shape[0])
+            eigvec_subspace = self.GS[key][:, -subspace_size:]
+            self.P[key] = eigvec_subspace
+
 
     def forward(self, X):
         self.a0 = X
@@ -143,17 +149,9 @@ class MLP(nn.Module):
         self.s2.retain_grad()
         return F.log_softmax(self.s2, dim=1)
 
-    '''
-    def forward(self, X):
-        X = F.relu(self.linear1(X))
-        X = F.relu(self.linear2(self.a1))
-        self.a2 = F.relu(self.s2)
-        self.s3 = self.linear3(self.a2)
-        self.s1.retain_grad()
-        self.s2.retain_grad()
-        self.s3.retain_grad()
-        return F.log_softmax(X, dim=1)
-    '''
+    def get_subspace_size(self, full_space_size, percent=0.1):
+        subspace_size = int(full_space_size * percent)
+        return subspace_size
 
     def get_grads(self):
         a0 = self.a0.detach().numpy()
@@ -162,48 +160,58 @@ class MLP(nn.Module):
         s1_grad = self.s1.grad.detach().numpy()
         a2 = self.a2.detach().numpy()
         s2_grad = self.s2.grad.detach().numpy()
-
         return (a0, s0_grad, a1, s1_grad, a2, s2_grad)
 
-    def projection_matrix_update(self, matrix, key, percent=0.1):
-        eigval, eigvec = np.linalg.eigh(matrix)
-        #plt.plot(eigval)
-        #plt.show()
-        num_eigvals = eigval.shape[0]
-        subspace_size = int(num_eigvals*percent)
-        eigvec_subspace = eigvec[:,-subspace_size:]
-        #print('Shape of P[{}] = {}'.format(key, eigvec_subspace.shape))
-        self.P[key] = eigvec_subspace
-        self.P[key] == np.eye(matrix.shape[0])
+    def projection_matrix_update(self):
+        for item_no, (key, item) in enumerate(self.GS.items()):
+            eigval, eigvec = np.linalg.eigh(self.GS[key])
+            subspace_size = self.get_subspace_size(eigvec.shape[0])
+            eigvec_subspace = eigvec[:, -subspace_size:]
+            self.P[key] = eigvec_subspace
 
-    def project_to_lower_space(self, matrix, key):
+
+    def project_vec_to_lower_space(self, matrix, key):
         #print('project_to_lower_space: Shape of P[{}] = {}. Shape of matrix = {}'.format(key, self.P[key].shape, matrix.shape))
         return matrix @ self.P[key]
 
-    def project_to_higher_space(self, matrix, key):
+    def project_vec_to_higher_space(self, matrix, key):
         #print('project_to_higher_space: Shape of P[{}] = {}. Shape of matrix = {}'.format(key, self.P[key].shape, matrix.shape))
         return self.P[key] @ matrix
 
+    def project_mtx_To_higher_space(self, matrix, key):
+        #print('self.P[{}].shape = {}, matrix.shape = {}'.format(key, self.P[key].shape, matrix.shape))
+        return self.P[key] @ matrix @ self.P[key].T
 
     def maintain_avgs(self, params):
         corr_curr = [None]*len(params)
-        for item_no, item in enumerate(params):
+        corr_curr_lower = [None] * len(params)
+        for item_no, (key, item) in enumerate(self.GS.items()):
             corr_curr[item_no] = params[item_no].T @ params[item_no]
+            corr_curr_lower_proj = self.project_vec_to_lower_space(params[item_no], key)
+            corr_curr_lower[item_no] = corr_curr_lower_proj.T @ corr_curr_lower_proj
         alpha = 0.95
         for item_no, (key, item) in enumerate(self.GS.items()):
             #print('corr_curr[{}].shape = {}, GS.shape[{}] = {}'.format(item_no, corr_curr[item_no].shape, key, self.GS[key].shape))
             self.GS[key] = alpha * self.GS[key] + (1 - alpha) * corr_curr[item_no]
+            self.GSLOWER[key] = alpha * self.GSLOWER[key] + (1 - alpha) * self.GSLOWER[key]
+
 
     def get_inverses(self):
         for item_no, (key, item) in enumerate(self.GS.items()):
-            self.projection_matrix_update(self.GS[key], key)
-            GSPROJ = self.project_to_lower_space(self.GS[key], key)
-            GSPROJINV = np.linalg.pinv(GSPROJ)# + np.eye(GSPROJ.shape[0]) * 0.001)
-            self.GSINV[key] = self.project_to_higher_space(GSPROJINV, key)
+            GSPROJINV = np.linalg.inv(self.GSLOWER[key])# + np.eye(GSPROJ.shape[0]) * 0.001)
+            self.GSINV[key] = self.project_mtx_To_higher_space(GSPROJINV, key)
+
+            #self.projection_matrix_update(self.GS[key], key)
+            #GSPROJ = self.project_to_lower_space(self.GS[key], key)
+            #GSPROJINV = np.linalg.pinv(GSPROJ)# + np.eye(GSPROJ.shape[0]) * 0.001)
+            #self.GSINV[key] = self.project_to_higher_space(GSPROJINV, key)
 
 
-def train(args, model, device, train_loader, optimizer, epoch, cnn_model=False):
+def train(args, model, device, train_loader, optimizer, epoch, train_loss_list, train_accuracy_list, cnn_model=False):
     model.train()
+    running_loss = 0
+    correct = 0
+
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         if not cnn_model:
@@ -211,21 +219,30 @@ def train(args, model, device, train_loader, optimizer, epoch, cnn_model=False):
         #print('data size = {}, target size = {}'.format(data.shape, target.shape))
         optimizer.zero_grad()
         output = model(data)
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
 
         loss = F.nll_loss(output, target)
         loss.backward()
+        running_loss += loss.item()
 
-        params = model.get_grads()
-        model.maintain_avgs(params)
-        if batch_idx % 50 == 0:
-            model.get_inverses()
-
-        optimizer.step(whitening_matrices=model.GSINV)
+        if isinstance(optimizer, NGD):
+            params = model.get_grads()
+            model.maintain_avgs(params)
+            if batch_idx % 50 == 0:
+                model.projection_matrix_update()
+                model.get_inverses()
+            optimizer.step(whitening_matrices=model.GSINV)
+        else:
+            optimizer.step()
 
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+
+    train_loss_list.append(running_loss/len(train_loader.dataset))
+    train_accuracy_list.append(100. * correct / len(train_loader.dataset))
 
 
 def test(model, device, test_loader, test_loss_list, accuracy_loss_list, cnn_model=False):
@@ -251,6 +268,21 @@ def test(model, device, test_loader, test_loss_list, accuracy_loss_list, cnn_mod
         100. * correct / len(test_loader.dataset)))
 
 
+def select_optimizer(model, optimizer_arg, lr):
+    if optimizer_arg == 'sgd':
+        optimizer = optim.SGD(model.parameters(), lr=lr)
+    elif optimizer_arg == 'adadelta':
+        optimizer = optim.Adadelta(model.parameters(), lr=lr)
+    elif optimizer_arg == 'ngd':
+        optimizer = NGD(model.parameters(), lr=lr)
+    return optimizer
+
+
+def save_files(loss_list, tag, suffix):
+    loss_list_np = np.array(loss_list)
+    loss_filename = 'temp/' + tag + suffix + 'txt'
+    loss_list_np.tofile(loss_filename, '\n', '%f')
+
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
@@ -274,9 +306,12 @@ def main():
                         help='For Saving the current Model')
     parser.add_argument('--cnn-model', action='store_true', default=False,
                         help='Use CNN model now')
+    parser.add_argument('--optimizer', type=str, default='sgd', help='Optimizer to Use')
     args = parser.parse_args()
     test_loss_list = []
-    accuracy_loss_list = []
+    test_accuracy_list = []
+    train_loss_list = []
+    train_accuracy_list = []
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
@@ -301,29 +336,43 @@ def main():
         model = Net().to(device)
     else:
         model = MLP().to(device)
-    #optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-    #optimizer = optim.SGD(model.parameters(), lr=args.lr)
-    optimizer = NGD(model.parameters(), lr=args.lr)
+
+    optimizer = select_optimizer(model, args.optimizer, args.lr)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch, cnn_model=args.cnn_model)
-        test(model, device, test_loader,  test_loss_list, accuracy_loss_list, cnn_model=args.cnn_model)
+        train(args, model, device, train_loader, optimizer, epoch, train_loss_list, train_accuracy_list, cnn_model=args.cnn_model)
+        test(model, device, test_loader,  test_loss_list, test_accuracy_list, cnn_model=args.cnn_model)
         scheduler.step()
 
     plt.subplot(211)
-    plt.plot(range(len(test_loss_list)), test_loss_list, 'r', label='test loss list')
-    plt.subplot(212)
-    plt.plot(range(len(accuracy_loss_list)), accuracy_loss_list, 'r', label='accuracy loss list')
+    plt.plot(range(len(test_loss_list)), test_loss_list, 'r', label='test loss')
+    plt.plot(range(len(train_loss_list)), train_loss_list, 'b', label='train loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
     plt.legend(loc=2, fontsize="small")
-    plt.show()
+
+    plt.subplot(212)
+    plt.plot(range(len(test_accuracy_list)), test_accuracy_list, 'r', label='test accuracy')
+    plt.plot(range(len(train_accuracy_list)), train_accuracy_list, 'b', label='train accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(loc=2, fontsize="small")
+
     now = datetime.now()
     date_time = now.strftime("%m%d%Y%H%M%S")
 
-    plt.savefig('eig_vals.png')
+    suffix = date_time + '_' + args.optimizer + '_lr' + str(args.lr) + '_gamma' + str(args.gamma)
+
     if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn" + date_time + ".pt")
-    np.save('temp/losses_' + data_time +'.npy', np.array(test_loss_list))
-    np.save('temp/accuracy_' + data_time + '.npy', np.array(accuracy_loss_list))
+        torch.save(model.state_dict(), "mnist_cnn" + suffix + ".pt")
+
+
+    plt.savefig('plot' + suffix + '.png')
+    save_files(test_loss_list, 'test_loss', suffix)
+    save_files(test_accuracy_list, 'test_accuracy', suffix)
+    save_files(train_loss_list, 'train_loss', suffix)
+    save_files(test_accuracy_list, 'train_accuracy', suffix)
 
 if __name__ == '__main__':
     main()
