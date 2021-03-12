33cf543bbd44167f675e993ac09ab14cd2038dae
Piyush Kaul <piyushkaul@gmail.com>
2021-03-12 15:49:53 +0530

    Plotting logic added.

--- scripts/plot_performance.py
+++ scripts/plot_performance.py
@@ -7,20 +7,31 @@ dirname='temp/'
 prefixes=['elapsed_time', 'test_accuracy', 'train_accuracy', 'train_loss', 'test_loss']
 suffix='2021_03_11_01_32_01_adam_ngd_lr_0.1_gamma_0.9_frac_1.0_fashion_mnist_direct_inv_period_50_proj_period_50_model_mlp_epochs_15_batch_size_64txt'
 suffix='2021_03_11_14_45_57_adam_ngd_lr_0.01_gamma_0.9_frac_1.0_fashion_mnist_direct_inv_period_500_proj_period_50_model_mlp_epochs_15_batch_size_64txt'
-params = {}
-for prefix in prefixes:
-    filename = dirname + prefix + suffix
-    with open(filename, 'r') as fp:
-        params[prefix] = np.fromfile(fp, dtype=np.float, sep='\n')
-        #print(prefix)
-        #print(params)
 
-for idx, (params_name, val) in enumerate(params.items()):
-    if idx==0:
-        continue
-    plt.subplot(511+int(idx))
-    plt.plot(range(len(params[params_name])), params[params_name], 'r', label=params_name)
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend(loc=2, fontsize="small")
+suffixes=[#'2021_03_12_01_40_24_adam_lr_0.001_gamma_0.8_frac_1_fashion_mnist_direct_inv_period_50_proj_period_50_model_mlp_epochs_15_batch_size_64txt',
+          #'2021_03_11_18_10_03_adam_ngd_lr_0.001_gamma_0.8_frac_1.0_fashion_mnist_recursive_inv_period_50_proj_period_50_model_mlp_epochs_15_batch_size_64txt',
+          '2021_03_12_10_50_58_adam_ngd_lr_0.001_gamma_0.8_frac_1.0_fashion_mnist_direct_inv_period_1_proj_period_1_model_mlp_epochs_15_batch_size_64txt',
+          '2021_03_12_11_53_10_adam_ngd_lr_0.001_gamma_0.8_frac_1.0_fashion_mnist_recursive_inv_period_500_proj_period_500_model_mlp_epochs_15_batch_size_64txt']
+
+params = {}
+for suffix in suffixes:
+    params[suffix] = {}
+
+for suffix in suffixes:
+    for prefix in prefixes:
+        filename = dirname + prefix + suffix
+        with open(filename, 'r') as fp:
+            params[suffix][prefix] = np.fromfile(fp, dtype=np.float, sep='\n')
+            #print(prefix)
+            #print(params)
+
+for suffix in suffixes:
+    for idx, (params_name, val) in enumerate(params[suffix].items()):
+        if idx==0:
+            continue
+        plt.subplot(511+int(idx))
+        plt.plot(params[suffix]['elapsed_time'][1:], params[suffix][params_name], 'r', label=params_name)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend(loc=2, fontsize="small")
 plt.show()
